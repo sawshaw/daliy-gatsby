1146d405500fbf5ce63c987581e663ef897ecffb
sawshaw <843506585@qq.com>
2018-09-17 16:18:37 +0800

    改首页
    描述

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,28 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/favicon.png'
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
+          首页
         </Link>
       </div>
       <div className="navbar-start">
         <Link className="navbar-item" to="/about">
           关于
         </Link>
-        <Link className="navbar-item" to="/products">
+        <Link className="navbar-item" to="/products"
           产品
         </Link>
       </div>
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://github.com/sawshaw"
           target="_blank"
           rel="noopener noreferrer"
         >
